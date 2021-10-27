d62144e68d48b445ac733616dbeb1502f6cbab1a
mulbricht <ulbricht@innoroute.de>
2021-10-28 00:56:30 +0100

    update

--- build/app.py
+++ build/app.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, "..")
 import time
 import os
+from rt_hat import SIGGEN as RT_HAT_SIGGEN
 
 from opcua import ua, Server
 
@@ -12,6 +13,7 @@ def set_speed(parent,new_speed):
 	motor_speed=new_speed.Value
 	print("Set new motor speed: "+str(motor_speed))
 	root.get_child(["0:Objects", "0:PiccoloNode", "0:motor", "0:speed"]).set_value(motor_speed)
+	RT_HAT_SIGGEN.set(0.5,int(motor_speed)*1000,0,0)
 #	os.popen("docker-compose "+name+" start")
 	return ""
 
@@ -31,6 +33,7 @@ if __name__ == "__main__":
     root = server.get_root_node()
     server.link_method(root.get_child(["0:Objects", "0:PiccoloNode", "0:motor", "0:set_speed"]),set_speed)
     server.start()
+    RT_HAT_SIGGEN.init("/hat_env.sh")
     
     try:
         count = 0
--- pushbuild.sh
+++ pushbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 datum=$(date +%s)
-docker build -t innoroute/tsnpi_opcua:$datum ./build/
-docker push innoroute/tsnpi_opcua:$datum
+docker build -t innoroute/tsnpi_opcua:latest ./build/
+docker push innoroute/tsnpi_opcua:latest
 #docker buildx build --push --platform linux/arm/v7  --tag innoroute/tsnpi_opcua:$datum ./build/
